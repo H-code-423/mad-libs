181c6be39d6120b5a24a05ecb80f1cef0a4778c4
Holly <68656446+H-code-423@users.noreply.github.com>
2020-08-18 00:17:29 -0400

    Add files via upload

---
+++ MAD LIBS GAME.py
@@ -0,0 +1,13 @@
+negative_emotion = input("Enter a negative emotion: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+friend = input("Enter a friends name: ")
+
+print("")
+
+print("")
+
+print("Roses are " + negative_emotion + ",")
+print(plural_noun + " are blue,")
+print("I love " + celebrity + ",")
+print("and " + friend + " smells like poo poo.")
